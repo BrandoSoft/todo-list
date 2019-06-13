a50d6475146ef80f1d3290b250aab5d369c5ea8c
BrandoSoft <csbrandox@gmail.com>
2019-06-13 21:36:06 +0200

    added some logic and skeleton, now going to sleep while it is so late...

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                HEllo from the  item
+
+                Stopped in 17:04
+            </div>
+        )
+    }
+}
