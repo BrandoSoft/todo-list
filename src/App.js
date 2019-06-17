b2d2b9d12037a5bae260e7a1d30b255ec8a5243b
BrandoSoft <csbrandox@gmail.com>
2019-06-17 22:11:23 +0200

    adding items working now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
@@ -6,18 +6,60 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from 'uuid';
 
 
-function App() {
+class App extends Component {
+  state={
+    items:[],
+    id: uuid(),
+    item:'',
+    editItem: false
+  }
+handleChange = e =>{
+  this.setState({
+    item:e.target.value
+  });
+}
+handleSubmit= e =>{
+  e.preventDefault();
+
+  const newItem = {
+    id:this.state.id,
+    title:this.state.item
+  };
+console.log(newItem);
+
+  const updatedItems = [...this.state.items, newItem];
+
+
+  this.setState({
+    items:updatedItems,
+    item: '', 
+    id: uuid(),
+    editItem:false
+  });
+
+};
+
+  render() {
   return (    
     <div className="container">
       <div className="row">
          <div className="col-10 mx-auto col-md-8 mt-4"> 
           <h3 className="text-capitalize text-center">todo input</h3>
-          <TodoInput />
-          <TodoList />
+          <TodoInput 
+            item={this.state.item} 
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
+          <TodoList 
+          items={this.state.items}/>
         </div>
       </div>
     </div>
   );
 }
+}
 
 export default App;
+
+
+50:00 a
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
     render() {
+        const {item, handleChange, handleSubmit} =this.props;
         return (
         <div className="card card-body my-3">
-            <form>
+            <form onSubmit={handleSubmit}>
                <div className="input-group">
                    <div className="imput-group-prepend">
                     <div className="input-group-text bg-primary text-white">
@@ -12,7 +13,9 @@ export default class TodoInput extends Component {
                     </div>
                    </div>
                    <input type="text" className="form-control text-capitalize" 
-                   placeholder="add a todo item" />
+                   placeholder="add a todo item" 
+                   value={item}
+                   onChange={handleChange}/>
                </div> 
                <button type="submit"className="btn btn-block btn-primary mt-3">add item</button>
             </form>
